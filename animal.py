d73dbcdf04ab28153b64c92027ea833fae35d413
gtsofa <jlsnyule@yahoo.com>
2018-03-13 23:13:19 +0300

    feature - inherintance and method overloading

--- animal.py
+++ animal.py
@@ -60,5 +60,43 @@ cat = Animal("Borris", 33, 10, "Meow")
 
 print(cat.toString())
 
+# inheritance
+class Dog(Animal):
+    __owner = ""
+
+
+    # override the parent class constructor
+    def __init__(self, name, height, weight, sound, owner):
+        self.__owner = owner
+        super(Dog).__init__(self, name, height, weight, sound)
+
+    def set_owner(self, owner):
+        self.__owner = owner
+
+    def get_owner(self):
+        return self.__owner
+
+    def get_type(self):
+        print("Dog")
+
+    # method overloading - overloaded the parent class's
+    def toString(self):
+        return "{} is {} cm tall and {} kilograms and say {} His owner is {}".format(self.__name, self.__height, self.__weight, self.__sound, self.__owner)
+
+    def multiple_sounds(self, how_many=None):
+        if how_many is None:
+            print(self.get_sound())
+
+        else:
+            print(self.get_sound() * how_many)
+
+# create Dog object now to test the dog class
+spot = Dog('Spot', 53, 27, 'Ruff', 'Julio')
+
+print(spot)
+
+
+
+
 
     
